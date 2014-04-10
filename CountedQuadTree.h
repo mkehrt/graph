3141020b376ea0598367ad4c2fc0a252d3422c63
Matthew Kehrt <mkehrt@roseandsigil.net>
2014-04-10 02:55:30 -0400

    shared_ptr attempt

--- CountedQuadTree.h
+++ CountedQuadTree.h
@@ -1,27 +1,32 @@
 #ifndef _COUNTED_QUAD_TREE_H_
 #define _COUNTED_QUAD_TREE_H_
 
+#include <memory>
 #include <utility>
+#include <vector>
 
 #include "Number.h"
 
-typedef std::pair<Number, Number> Point;
-
 class CountedQuadTree {
-
+  typedef std::pair<Number, Number> Point;
 };
 
 class CountedQuadTreeNode : public CountedQuadTree {
 private:
-  CountedQuadTree* nw;
-  CountedQuadTree* ne;
-  CountedQuadTree* sw;
-  CountedQuadTree* se;
+  int count;
+
+  const std::shared_ptr<CountedQuadTree> nw;
+  const std::shared_ptr<CountedQuadTree> ne;
+  const std::shared_ptr<CountedQuadTree> sw;
+  const std::shared_ptr<CountedQuadTree> se;
 };
 
 class CountedQuadTreeLeaf : public CountedQuadTree {
 private:
+  static const int maxPoints = 256;
+  std::vector<point> points;
 
+  const std::shared_ptr<CountedQuadTree> split();
 };
 
 #endif // _COUNTED_QUAD_TREE_H_

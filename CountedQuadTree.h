afceac3e9e15bc987a15487ba7bbdb9c3eb495aa
Matthew Kehrt <mkehrt@roseandsigil.net>
2014-04-10 02:17:47 -0400

    cqt started

---
+++ CountedQuadTree.h
@@ -0,0 +1,15 @@
+#ifndef _COUNTED_QUAD_TREE_H_
+#define _COUNTED_QUAD_TREE_H_
+
+#include <utility>
+
+#include "Number.h"
+
+class CountedQuadTree {
+
+private:
+  typedef std::pair<Number, Number> Point;
+
+};
+
+#endif // _COUNTED_QUAD_TREE_H_

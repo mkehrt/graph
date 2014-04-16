505504907349e7ccfddf15d207228db2fda3070f
Matthew Kehrt <mkehrt@roseandsigil.net>
2014-04-15 23:00:04 -0400

    tyepdefs &c

--- CountedQuadTree.h
+++ CountedQuadTree.h
@@ -7,35 +7,43 @@
 
 #include "Number.h"
 
+typedef const std::pair<const Number, const Number> Point;
+typedef const std::pair<const Point, const Point> Rect;
+
 class CountedQuadTree {
-protected:
-  typedef std::pair<Number, Number> Point;
+public:
+  typedef const std::shared_ptr<const CountedQuadTree> T;
+ 
+  static T fromPoints(const std::vector<Point>&);
+
+  bool contains(const Point &) const;
+  T cover(const Rect &) const;
+
 private:
   CountedQuadTree();
   CountedQuadTree(const CountedQuadTree&);
-};
 
-class CountedQuadTreeNode : public CountedQuadTree {
-private:
   int count;
-
-  const std::shared_ptr<CountedQuadTree> nw;
-  const std::shared_ptr<CountedQuadTree> ne;
-  const std::shared_ptr<CountedQuadTree> sw;
-  const std::shared_ptr<CountedQuadTree> se;
-};
-
-class CountedQuadTreeRoot : public CountedQuadTreeNode {
-private:
   Number xMin, yMin, xMax, yMax;
 };
 
+
+class CountedQuadTreeNode : public CountedQuadTree {
+private:
+  T nw;
+  T ne;
+  T sw;
+  T se;
+};
+
 class CountedQuadTreeLeaf : public CountedQuadTree {
+public:
+  T split();
+
 private:
   static const int maxPoints = 256;
   std::vector<Point> points;
 
-  const std::shared_ptr<CountedQuadTree> split();
 };
 
 #endif // _COUNTED_QUAD_TREE_H_
